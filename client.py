22e6a29b5fa2e6d3544240d7bbc3ca2655504e81
Matthew Pull <thematthewpull@gmail.com>
2017-10-26 20:09:14 +0100

    Include aggregate

--- client.py
+++ client.py
@@ -41,16 +41,54 @@ def handle_aggregated(challenge, analyser):
     sentiments = {}
     # Just guess
     min_time = min(t.time for t in challenge.tweets)
-    max_time = max(t.time for t in challenge.tweets)
-    known_subjects = ["Motionmart", "Spherebank", "Purpleworth"]
+    max_time = max(t.time for t in challenge.tweets) + 1
+    companies = webhandler.get_company_info()
+    known_subjects = []
+
+    for company in companies:
+        known_subjects.append(company.name)
+
+    subject_sentiments = {}
 
     for subject in known_subjects:
-        subject_sentiments = {}
+        subject_sentiments[subject] = {}
         for i in range(min_time, max_time):
-            subject_sentiments[i] = random.randrange(-1, 1)
-        sentiments[subject] = subject_sentiments
+            subject_sentiments[subject][i] = []
+
+    print(subject_sentiments)
+
+    for tweet in challenge.tweets:
+        tweet_analysis = analyser.analyse_tweet(tweet.tweet)
+        for (subject, sentiment) in tweet_analysis:
+            subject_sentiments[subject][tweet.time].append(sentiment)
+
+    print(subject_sentiments)
+
+    for subject, times in subject_sentiments.items():
+        sentiments[subject] = {}
+        useSubject = False
+        for time, scores in times.items():
+            length = 0
+            total = 0
+            for score in scores:
+                useSubject = True
+                length += 1
+                total += score
+            try:
+                sentiments[subject][time] = total / length
+            except ZeroDivisionError:
+                sentiments[subject][time] = 0
+        if not useSubject:
+            del sentiments[subject]
+
+    # for subject in known_subjects:
+    #     subject_sentiments = {}
+    #     for i in range(min_time, max_time):
+    #         subject_sentiments[i] = random.randrange(-1, 1)
+    #     sentiments[subject] = subject_sentiments
 
     submission = {'challengeId': challenge.info.cid, 'sentiments': sentiments}
+    print(submission)
     result = webhandler.post_aggregated_submission(submission)
     print ("Mark = {}%".format(result.mark))
 
